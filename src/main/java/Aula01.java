c6a41e56a152460bc7cd3b3aa2dd216a47a4fc7f
davidncuba <david@holdcb.com>
2021-02-09 22:15:36 -0300

    Aula01
    Aula01

---
+++ src/main/java/Aula01.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author davidcuba
+ */
+public class Aula01 {
+
+    public static void main (String[] args) {
+        System.out.println("Sejam bem vindos ao 2º Ano de CC - UNIP Bauru");
+        System.out.println("Iniciando estudos no Java");
+        
+        Aula01 obj = new Aula01();
+        
+        obj.funcao1();
+        obj.funcao2();
+    }
+    
+    public void funcao1() {
+        System.out.println("Estou na função 1");
+    }
+    
+    public void funcao2() {
+        System.out.println("Estou na função 2");        
+    }
+}
